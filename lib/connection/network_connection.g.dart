5fc65c29745404f1e6798e3df5db7dc9a768fc6a
ReshmaSathyan <reshusathyan@gmail.com>
2022-12-27 16:27:13 +0530

    doing integration

---
+++ lib/connection/network_connection.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'network_connection.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _NetworkConnection implements NetworkConnection {
+  _NetworkConnection(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'https://wpr.intertoons.net/d2dApi/';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
